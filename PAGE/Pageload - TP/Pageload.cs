c0f41ec2a54f05455dc821f886425e12787cf893
USWATTA PERERA <troy1lk@gmail.com>
2020-08-04 13:08:23 +0100

    pageload

---
+++ PAGE/Pageload - TP/Pageload.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using SeleniumExtras.PageObjects;
+
+namespace SHORTUPLOADS.PAGE.Pageload___TP
+{
+    public class Pageload
+    {
+        public IWebDriver Driver;
+        public Pageload (IWebDriver driver) 
+        {
+            Driver = driver;
+            PageFactory.InitElements(Driver, this);
+        }
+
+        public void NavigateMethod() 
+        {
+            Driver.Navigate().GoToUrl("http://shortuploads.phpdevelopersinindia.in/");
+        }
+
+        public void VerifyPageload() 
+        {
+            IWebElement VPL = Driver.FindElement(By.Id("navbarDropdown"));
+            VPL.Displayed.Should().BeTrue();
+        }
+            
+        
+    }
+}
---
+++ TEST/Pageload - TP/PageLoad.feature
@@ -0,0 +1,9 @@
+﻿Feature: PageLoad
+	In order to verify the page load function
+	As a user
+	I want to go to Shortuploads home page
+
+@pageload
+Scenario: Verify Page load
+	Given I navigate to Shortuploads
+	Then I see the videos
