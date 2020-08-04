e891e20e0d98f4d5ca506538ba04370298f2bef9
USWATTA PERERA <troy1lk@gmail.com>
2020-08-05 00:05:17 +0100

    Login

---
+++ STEP/Login - TP/LoginSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using SHORTUPLOADS.BASE;
+using SHORTUPLOADS.PAGE.Login__TP;
+using OpenQA.Selenium;
+
+namespace SHORTUPLOADS.STEP.Login___TP
+{
+    [Binding]
+    [Scope(Tag ="login")]
+    public class LoginSteps : Hooks
+    {
+        public IWebDriver driver;
+        public Login browser;
+
+        [Given(@"I navigate to shortuploads")]
+        public void GivenINavigateToShortuploads()
+        {
+            driver = Driver;
+            browser = new Login(driver);
+            browser.NavigateMethod();
+        }
+        
+        [When(@"I click login")]
+        public void WhenIClickLogin()
+        {
+            browser.ClickLogin();
+        }
+        
+        [When(@"I enter login details")]
+        public void WhenIEnterLoginDetails()
+        {
+            browser.EnterLoginDetails();
+        }
+        
+        [Then(@"I am signed in to shortuploads")]
+        public void ThenIAmSignedInToShortuploads()
+        {
+            browser.VerifyLogin();
+        }
+    }
+}
---
+++ TEST/Login - TP/Login.feature
@@ -0,0 +1,11 @@
+﻿Feature: Login
+	In order to login Shortuploads
+	As a user
+	I want to be able to login Shortuploads website
+
+@login
+Scenario: Verify Login
+	Given I navigate to shortuploads
+	When I click login
+	And I enter login details
+	Then I am signed in to shortuploads
